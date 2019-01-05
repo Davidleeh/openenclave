99cd049b08d5b601f712cccf6c7f63105b3e9e46
Hernan Gatta <hegatta@microsoft.com>
2019-01-04 18:57:42 -0800

    EOD

--- new_platforms/cmake/FindWDK.cmake
+++ new_platforms/cmake/FindWDK.cmake
@@ -1,10 +1,20 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-if(DEFINED ENV{WDKContentRoot})
-    file(GLOB WDK_PATH "$ENV{WDKContentRoot}/Include/${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION}/km")
+# NOTE: Hosted Visual Studio 2017 instances in Azure DevOps for Continuous
+# Integration (CI) do not have the WDK installed. We work around this by having
+# CI download a custom WDK package that has the files we need, and nothing more
+# seeing as installing the WDK from the official package is not trivial. This
+# is what the WDKCIOverride environment variable is used for.
+
+if(DEFINED ENV{WDKCIOverride})
+    set(WDK_PATH ENV{WDKCIOverride})
 else()
-    file(GLOB WDK_PATH "$ENV{ProgramFiles\(x86\)}/Windows Kits/10/Include/${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION}/km")
+    if(DEFINED ENV{WDKContentRoot})
+        file(GLOB WDK_PATH "$ENV{WDKContentRoot}/Include/${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION}/km")
+    else()
+        file(GLOB WDK_PATH "$ENV{ProgramFiles\(x86\)}/Windows Kits/10/Include/${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION}/km")
+    endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
